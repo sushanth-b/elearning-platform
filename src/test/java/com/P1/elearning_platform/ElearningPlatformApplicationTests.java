5860858d5730e883f57cf07faa36e6a02eb61296
sushanth-b <sushanth.bobbala@gmail.com>
2026-09-25 13:38:46 +0530

    Initial project scaffold with Spring Boot, JPA, MySQL, Security dependencies

---
+++ src/test/java/com/P1/elearning_platform/ElearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.P1.elearning_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
